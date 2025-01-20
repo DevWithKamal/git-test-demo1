b0cf9965ca007f6c95f1e473900d6e21ad14888b
KamalNexdew <kamal.bajaj@nexdew.com>
2025-01-20 23:46:56 +0530

    init commit

---
+++ src/main/java/com/lcwd/withoutboot/gittestdemo1/GitTestDemo1Application.java
@@ -0,0 +1,13 @@
+package com.lcwd.withoutboot.gittestdemo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitTestDemo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitTestDemo1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/lcwd/withoutboot/gittestdemo1/GitTestDemo1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lcwd.withoutboot.gittestdemo1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitTestDemo1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
