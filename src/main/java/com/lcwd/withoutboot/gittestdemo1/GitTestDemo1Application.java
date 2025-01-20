88fbf6cd7f578df838c6e8f4052bac8c3e2575c8
KamalNexdew <kamal.bajaj@nexdew.com>
2025-01-20 23:50:27 +0530

    This is Kamal Bajaj

--- src/main/java/com/lcwd/withoutboot/gittestdemo1/GitTestDemo1Application.java
+++ src/main/java/com/lcwd/withoutboot/gittestdemo1/GitTestDemo1Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GitTestDemo1Application {
 
 	public static void main(String[] args) {
+		System.out.println("This is  Kamal Bajaj");
 		SpringApplication.run(GitTestDemo1Application.class, args);
 	}
 
